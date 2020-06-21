45fcaf9413af0af061b35499c3b4d7e836d07d30
Dmitriy Voronin <dmitriy.vrnn@gmail.com>
2020-06-21 04:02:48 +0300

    Updates

---
+++ precache-manifest.77f32e86fb6a2ac4c90d5a416bbd9ea9.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46bc5a0301cd0ad365b9bce2e261bfc3",
+    "url": "/vechain/index.html"
+  },
+  {
+    "revision": "9fe148c0b062fabb56d6",
+    "url": "/vechain/static/css/main.58dadbdd.chunk.css"
+  },
+  {
+    "revision": "176658eb55d488fb186a",
+    "url": "/vechain/static/js/2.2fd0dc4d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/vechain/static/js/2.2fd0dc4d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9fe148c0b062fabb56d6",
+    "url": "/vechain/static/js/main.5e3e1446.chunk.js"
+  },
+  {
+    "revision": "c4da47efabb565233824",
+    "url": "/vechain/static/js/runtime-main.8c863204.js"
+  },
+  {
+    "revision": "58aef543c97bbaf6a9896e8484456d98",
+    "url": "/vechain/static/media/Roboto-Medium.58aef543.ttf"
+  },
+  {
+    "revision": "d41d8cd98f00b204e9800998ecf8427e",
+    "url": "/vechain/static/media/Roboto-Regular.d41d8cd9.ttf"
+  },
+  {
+    "revision": "0a8cb0ef917d1728f1f2dbd54b74d302",
+    "url": "/vechain/static/media/arrow-lg.0a8cb0ef.svg"
+  },
+  {
+    "revision": "93fd670bdd2b85457e55a0ebdb4c110e",
+    "url": "/vechain/static/media/arrow-md.93fd670b.svg"
+  },
+  {
+    "revision": "7fac815b23d85b9195f82c9005da98bc",
+    "url": "/vechain/static/media/bg_v.7fac815b.svg"
+  },
+  {
+    "revision": "11c161a41b7ca210934696678b2b00e0",
+    "url": "/vechain/static/media/chevron.11c161a4.svg"
+  },
+  {
+    "revision": "f98cc9717a1e64bc3327c530cc432d89",
+    "url": "/vechain/static/media/figure.f98cc971.svg"
+  },
+  {
+    "revision": "9d241415a2daf30efda74459a092b2bf",
+    "url": "/vechain/static/media/figure_m.9d241415.svg"
+  },
+  {
+    "revision": "4a80dd91012088c6cf05a91d13cc610a",
+    "url": "/vechain/static/media/plus.4a80dd91.svg"
+  },
+  {
+    "revision": "559c61b8a7196cdbc899963e95b1d876",
+    "url": "/vechain/static/media/vector.559c61b8.svg"
+  }
+]);
