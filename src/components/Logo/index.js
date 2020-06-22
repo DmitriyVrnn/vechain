2e373d6d6a0192f42381511aef52ca7dcb2053af
Dmitriy Voronin <dmitriy.vrnn@gmail.com>
2020-06-22 22:34:46 +0300

    Fixes

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { ReactComponent as Plus } from "../../assets/img/plus.svg";
 import { ReactComponent as Vector } from "../../assets/img/vector.svg";
+import { ReactComponent as Change } from "../../assets/logo/change.svg";
+import { ReactComponent as Now } from "../../assets/logo/now.svg";
 
 import './styles.css';
 
@@ -9,8 +11,8 @@ export const Logo = () => {
   return (
     <div className="logo">
       <div className="logo__wrap-text">
-        <span>change</span>
-        <strong>NOW</strong>
+        <Change/>
+        <Now/>
       </div>
       <Plus/>
       <Vector/>
--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -8,7 +8,7 @@ export const Widget = () => (
       title="Exchange"
       src='https://changenow.io/embeds/exchange-widget/v2/widget.html?amount=0.1&from=btc&link_id=4738f5c74dcbe0&to=vet&FAQ=false&logo=true'
       width='100%'
-      height='350px'
+      height='370px'
       frameBorder='0'/>
     <script type='text/javascript' async src='https://changenow.io/embeds/exchange-widget/v2/stepper-connector.js'/>
   </div>
